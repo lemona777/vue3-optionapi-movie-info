a461356ce827eb396fa2282bd47839f6bc00a51c
jang <lemona9201@gmail.com>
2024-03-29 15:02:48 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,152 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <Event :text="text[eventTextNum]" />
+  <SearchBar :data="data_temp" @searchMovie="searchMovie($event)" />
+  <p>
+    <button @click="showAllMovie">전체보기</button>
+  </p>
+  <Movies 
+    :data="data_temp"
+    @openModal="isModal=true;selectedMovie=$event"
+    @increaseLike="increaseLike($event)"
+  />
+  <Modal 
+    :data="data" 
+    :isModal="isModal" 
+    :selectedMovie="selectedMovie" 
+    @closeModal="isModal=false"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import data from './assets/movies';
+  import Navbar from './components/Navbar.vue';
+  import Event from './components/Event.vue';
+  import Modal from './components/Modal.vue';
+  import Movies from './components/Movies.vue';
+  import SearchBar from './components/SearchBar.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        isModal: false,
+        data: data,
+        data_temp: [...data], // 사본
+        selectedMovie: 0,
+        text: [
+          '넷플릭스',
+          '디즈니',
+          '티빙',
+        ],
+        eventTextNum: 0,
+        interval: null,
+      }
+    },
+    methods: {
+      increaseLike(id) {
+        this.data.find(movie => {
+          if(movie.id == id) {
+            movie.like += 1;
+          }
+        })
+      },
+      searchMovie(title) {
+        // 영화제목이 포함된 데이터를 가져옴
+        this.data_temp = this.data.filter(movie => {
+          return movie.title.includes(title);
+        })
+      },
+      showAllMovie() {
+        this.data_temp = [...this.data];
+      }
+    },
+    components: {
+      Navbar: Navbar,
+      Modal: Modal,
+      Event: Event,
+      Movies: Movies,
+      SearchBar: SearchBar,
+    },
+    mounted() {
+      console.log('mounted');
+      this.interval = setInterval(() => {
+        if(this.eventTextNum == this.text.length-1){
+          this.eventTextNum = 0;
+        }else{
+          this.eventTextNum += 1;
+        }
+      }, 3000);
+    }, 
+    unmounted() {
+      clearInterval(this.interval);
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body {
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  h1, h2, h3 {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+
+  button {
+    margin-right: 10px;
+    margin-top: 1rem;
+  }
+
+  .item {
+    width: 100%;
+    border: 1px solid #ccc;
+    display: flex;
+    margin-bottom: 20px;
+    padding: 1rem;
+  }
+
+  .item figure {
+    width: 30%;
+    margin-right: 1rem;
+  }
+
+  .item img {
+    width: 100%;
+  }
+
+  .item .info {
+    width: 100%;
+  }
+
+  .modal {
+    background: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal .inner {
+    background: #fff;
+    width: 80%;
+    padding: 20px;
+    border-radius: 10px;
+  }
+
 </style>
---
+++ src/assets/movies.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+      id: 0,
+      title: "어벤져스",
+      year: 2023,
+      category: "액션, 드라마",
+      like: 0,
+      imgUrl: './assets/A1.jpg',
+    },
+    {
+      id: 1,
+      title: "더포스트",
+      year: 2024,
+      category: "멜로",
+      like: 0,
+      imgUrl: './assets/A2.jpg',
+    },
+    {
+      id: 2,
+      title: "포스트임팩트",
+      year: 2024,
+      category: "공포",
+      like: 0,
+      imgUrl: './assets/A3.jpg',
+    },
+  ]
+
+export default data;
